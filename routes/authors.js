0bbc2e4c639ed631a16c7d9a574725b9e866b62d
Brian <bstrick@gmail.com>
2021-05-03 19:25:12 -0400

    Created authors listing page AB#297

--- routes/authors.js
+++ routes/authors.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('You are at the authors listing');
 });
 
 module.exports = router;

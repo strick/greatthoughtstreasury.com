1ca0a4fdf334070fe041663e4884fe0c1a4a9855
Brian <bstrick@gmail.com>
2021-05-05 10:49:14 -0400

    building resource group AB#312

---
+++ gtt-web/tf/main.tf
@@ -0,0 +1,6 @@
+# Initialises Terraform providers and sets their version numbers.
+
+provider "azurerm" {
+    #version = "2.56.0"
+    features {}
+}
---
+++ gtt-web/tf/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+    default = "greatthoughtstreasury"
+}
+
+variable "location" {
+    default = "East US"
+}
+
+variable admin_username {
+  default = "linux_admin"
+}

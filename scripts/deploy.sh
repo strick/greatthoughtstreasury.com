89b08d976a523669850ce47c5903434c074f40c7
Brian <bstrick@gmail.com>
2021-05-07 13:15:11 -0400

    try azure files in azure AB#317

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,5 @@
 cd tf
 
-GTT_FS_SHARE=mongodata
-GTT_FS_ACCOUNT=gttmongodb
-GTT_FS_KEY=F5UUo5rsiZM7nyVE74LlJ8eiFEB1LhtYHcVH9x/WFivU38uFzvRmyagDI5LM0LjAAAf/OZv3ysw81DtoKWnKBQ==
-
 echo "terraform apply -auto-approve\"
     -var \"app_version=$VERSION\" 
     -var \"client_id=$ARM_CLIENT_ID\" 
@@ -12,19 +8,12 @@ echo "terraform apply -auto-approve\"
 eval "sed -i 's/MY_VERSION/$VERSION/' ../docker-compose.yaml"
 eval "sed -i 's/GTT_FS_SHARE/$GTT_FS_SHARE/' ../docker-compose.yaml"
 eval "sed -i 's/GTT_FS_ACCOUNT/$GTT_FS_ACCOUNT/' ../docker-compose.yaml"
-eval "sed -i 's/GTT_FS_KEY/$GTT_FS_KEY/' ../docker-compose.yaml"
+eval "sed -i 's,GTT_FS_KEY,$GTT_FS_KEY,' ../docker-compose.yaml"  # escapse the / in keys
 
 cat ../docker-compose.yaml
+echo 
 
-terraform init 
-terraform apply -auto-approve \
-    -var "app_version=$VERSION" \
-    -var "client_id=$ARM_CLIENT_ID" \
-    -var "client_secret=$ARM_CLIENT_SECRET" \
-    -var "tenant_id=$ARM_TENANT_ID" \
-    -var "subscription_id=$ARM_SUBSCRIPTION_ID"
-
-eval "sed -i 's/$VERSION/MY_VERSION/' ../docker-compose.yaml"
-eval "sed -i 's/$GTT_FS_SHARE/GTT_FS_SHARE/' ../docker-compose.yaml"
-eval "sed -i 's/$GTT_FS_ACCOUNT/GTT_FS_ACCOUNT/' ../docker-compose.yaml"
-eval "sed -i 's/$GTT_FS_KEY/GTT_FS_KEY/' ../docker-compose.yaml"
+#eval "sed -i 's/$VERSION/MY_VERSION/' ../docker-compose.yaml"
+#eval "sed -i 's/$GTT_FS_SHARE/GTT_FS_SHARE/' ../docker-compose.yaml"
+#eval "sed -i 's/$GTT_FS_ACCOUNT/GTT_FS_ACCOUNT/' ../docker-compose.yaml"
+#eval "sed -i 's,$GTT_FS_KEY,GTT_FS_KEY,' ../docker-compose.yaml"

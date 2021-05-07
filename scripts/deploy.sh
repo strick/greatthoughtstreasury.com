07c25125073bd232834deccea23c828a494ab3b3
Brian <bstrick@gmail.com>
2021-05-07 13:16:28 -0400

    left off the init files AB#317

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,15 @@ eval "sed -i 's/GTT_FS_ACCOUNT/$GTT_FS_ACCOUNT/' ../docker-compose.yaml"
 eval "sed -i 's,GTT_FS_KEY,$GTT_FS_KEY,' ../docker-compose.yaml"  # escapse the / in keys
 
 cat ../docker-compose.yaml
-echo 
+echo  
+
+terraform init 
+terraform apply -auto-approve \
+    -var "app_version=$VERSION" \
+    -var "client_id=$ARM_CLIENT_ID" \
+    -var "client_secret=$ARM_CLIENT_SECRET" \
+    -var "tenant_id=$ARM_TENANT_ID" \
+    -var "subscription_id=$ARM_SUBSCRIPTION_ID"
 
 #eval "sed -i 's/$VERSION/MY_VERSION/' ../docker-compose.yaml"
 #eval "sed -i 's/$GTT_FS_SHARE/GTT_FS_SHARE/' ../docker-compose.yaml"

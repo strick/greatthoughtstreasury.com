09f140299e5575178fe6636c7792f1c14ca09b60
Brian <bstrick@gmail.com>
2021-05-07 12:48:40 -0400

    try azure files in azure AB#317

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,18 @@
 cd tf
 
+GTT_FS_SHARE=mongodata
+GTT_FS_ACCOUNT=gttmongodb
+GTT_FS_KEY=F5UUo5rsiZM7nyVE74LlJ8eiFEB1LhtYHcVH9x/WFivU38uFzvRmyagDI5LM0LjAAAf/OZv3ysw81DtoKWnKBQ==
+
 echo "terraform apply -auto-approve\"
     -var \"app_version=$VERSION\" 
     -var \"client_id=$ARM_CLIENT_ID\" 
     -var \"client_secret=$ARM_CLIENT_SECRET\""
 
 eval "sed -i 's/MY_VERSION/$VERSION/' ../docker-compose.yaml"
+eval "sed -i 's/GTT_FS_SHARE/$GTT_FS_SHARE/' ../docker-compose.yaml"
+eval "sed -i 's/GTT_FS_ACCOUNT/$GTT_FS_ACCOUNT/' ../docker-compose.yaml"
+eval "sed -i 's/GTT_FS_KEY/$GTT_FS_KEY/' ../docker-compose.yaml"
 
 cat ../docker-compose.yaml
 
@@ -18,3 +25,6 @@ terraform apply -auto-approve \
     -var "subscription_id=$ARM_SUBSCRIPTION_ID"
 
 eval "sed -i 's/$VERSION/MY_VERSION/' ../docker-compose.yaml"
+eval "sed -i 's/$GTT_FS_SHARE/GTT_FS_SHARE/' ../docker-compose.yaml"
+eval "sed -i 's/$GTT_FS_ACCOUNT/GTT_FS_ACCOUNT/' ../docker-compose.yaml"
+eval "sed -i 's/$GTT_FS_KEY/GTT_FS_KEY/' ../docker-compose.yaml"

f621dd94f3ce4e79bdbc40f46be4a5ccc428c9b8
Brian <bstrick@gmail.com>
2021-05-06 22:54:13 -0400

    Attempting new variable passing AB#317

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,4 +13,3 @@ terraform apply -auto-approve \
     -var "client_secret=$ARM_CLIENT_SECRET" \
     -var "tenant_id=$ARM_TENANT_ID" \
     -var "subscription_id=$ARM_SUBSCRIPTION_ID"
-    
--- tf/asp.tf
+++ tf/asp.tf
@@ -94,11 +94,12 @@ resource "azurerm_app_service" "asp" {
   app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
 
   site_config {
-    app_command_line = ""
-    linux_fx_version = "COMPOSE|${filebase64("../docker-compose.yaml")}"
+    app_command_line = "export"
+    linux_fx_version = "VERSION=3333|COMPOSE|${filebase64("../docker-compose.yaml")}"
   }
 
   app_settings = {
+    "VERSION"   = "HELLO",
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false",
     "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.container_registry.admin_username,
     "DOCKER_REGISTRY_SERVER_URL" = azurerm_container_registry.container_registry.login_server,

d86377edc227bd6ca8db578fb8a46691b5566c85
Brian <bstrick@gmail.com>
2021-05-07 23:51:52 -0400

    All authors are importing AB#308

--- gtt-web/src/bin/import-data.js
+++ gtt-web/src/bin/import-data.js
@@ -1,14 +1,20 @@
 'use strict'
 
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+console.log(process.env);
 const Author = require('../models/Author');
 var Quote = require('../models/Quote');
 var db = require('../db');
+console.log(process.env);
 var t = 1;
   // create a client to mongodb
 var MongoClient = require('mongodb').MongoClient;
 
 var fs = require("fs");
 console.log("Reading Authors file");
+
 // Open a new file with name input.txt and write Simply Easy Learning! to it.
 try {
     const data = fs.readFileSync(__dirname + '/import-files/authors.txt', 'UTF-8');
@@ -36,8 +42,17 @@ try {
     });
     console.log("Author count: " + count);
     console.log(authors);
-    //Author.insertMany(authors);
 }
 catch(err){
     console.error(err);
 }
+
+
+(async function(){
+    console.log("Connecting");
+    await db.connect();
+    console.log("Inserting")
+    await Author.insertMany(authors);
+    console.log("closing");
+    db.close();
+})();

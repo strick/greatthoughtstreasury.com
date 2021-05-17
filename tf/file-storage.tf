89a786606e0590e2ba65854e62e9082118e655e3
Brian <bstrick@gmail.com>
2021-05-17 16:04:13 -0400

    Added the blob and container AB#360

--- tf/file-storage.tf
+++ tf/file-storage.tf
@@ -6,3 +6,16 @@ resource "azurerm_storage_account" "gtt_file_storage" {
   account_replication_type = "GRS"  # $0.02 more than LRS
 
 }
+
+resource "azurerm_storage_container" "gttfilestorage" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.greatthoughtstreasury.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "gttauthorimages" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+}

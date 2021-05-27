22735569a63cedc8b3ed54561e18c31c1fa50d57
Brian <bstrick@gmail.com>
2021-05-27 19:58:03 -0400

    jest will exit now on failures AB#376

--- gtt-web/src/controllers/author.test.js
+++ gtt-web/src/controllers/author.test.js
@@ -14,16 +14,22 @@ describe('Author controller requests', () => {
     // ToDo - MOCK this database so you can supply the value and confirm the page gives it back
     it('should return one author when existing id supplied', async () => {
         await request(app)
-        .get('/authors/60a2a8757036d21c9f76ac8e')
+        .get('/authors/60a2a8757036d21c9f76d024')
         .expect(200)
         .expect(function(res){
-            if(!res.text.includes("John  Abbott, fully John Stevens Cabot Abbott")) throw new Error("Expected to find the author");
+            if(!res.text.includes("John O’Donohue")) throw new Error("Expected to find the author");
         });
     });
 
     it('should return 404 on undefined author', async () => {
         await request(app)
-        .get('/authors/dafsdfsdk3333')
+        .get('/authors/single/dafsdfsdk3333')
         .expect(404);
     })
 });
+
+afterAll(done => {
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose.connection.close()
+    done()
+  })

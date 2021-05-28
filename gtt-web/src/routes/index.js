5fd1874e7a970a18ca165f26767e2e4f4741de00
Brian <bstrick@gmail.com>
2021-05-28 14:49:40 -0400

    fix ssl AB#402

--- gtt-web/src/routes/index.js
+++ gtt-web/src/routes/index.js
@@ -6,12 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Great Thoughts Treasury' });
 });
 
-router.get('/.well-known/acme-challenge/umDtPtH6w7ySKyL8-JjD7vN5_yoUje4iJABNZlvRFJk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U', function(req, res, next) {
+router.get('/.well-known/acme-challenge/umDtPtH6w7ySKyL8-JjD7vN5_yoUje4iJABNZlvRFJk', function(req, res, next) {
   res.set('Content-Type', 'text/plain');
   res.send("umDtPtH6w7ySKyL8-JjD7vN5_yoUje4iJABNZlvRFJk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U");
 });
 
-router.get('/.well-known/acme-challenge/tHXmQpThj_1BmFfOhTaQKDqIpDMkrtsbdhQujb2mSUk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U', function(req, res, next) {
+http://greatthoughtstreasury.com/.well-known/acme-challenge/umDtPtH6w7ySKyL8-JjD7vN5_yoUje4iJABNZlvRFJk
+router.get('/.well-known/acme-challenge/tHXmQpThj_1BmFfOhTaQKDqIpDMkrtsbdhQujb2mSUk', function(req, res, next) {
   res.set('Content-Type', 'text/plain');
   res.send("tHXmQpThj_1BmFfOhTaQKDqIpDMkrtsbdhQujb2mSUk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U");
 });

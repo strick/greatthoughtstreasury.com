a87dbc6ff0fcddbee6c4bef443f92ca43f78f301
Brian <bstrick@gmail.com>
2021-05-28 14:40:26 -0400

    new ssl endpoint AB#402

--- gtt-web/src/routes/index.js
+++ gtt-web/src/routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/ssl1', function(req, res, next) {
-  res.render('index', { title: 'Great Thoughts Treasury' });
+  res.set('Content-Type', 'text/plain');
+  res.send("umDtPtH6w7ySKyL8-JjD7vN5_yoUje4iJABNZlvRFJk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U");
 });
 
 router.get('/.well-known/acme-challenge/tHXmQpThj_1BmFfOhTaQKDqIpDMkrtsbdhQujb2mSUk.3DCjAQOmsihzQajSyJD7dF2ol_wTVzRfSeX4NV4p26U', function(req, res, next) {

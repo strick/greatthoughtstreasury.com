8dd87b28a13171529b6473aa0e6de7f9e145021c
Brian <bstrick@gmail.com>
2021-06-01 18:20:52 -0400

    cache the authors and topics AB#416

--- gtt-web/src/routes/authors.js
+++ gtt-web/src/routes/authors.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var cache = require('../middleware/cache');
 var controller = require('../controllers/author');
 var router = express.Router();
 
-router.route('/').get(controller.listAllPage);
-router.route('/:page').get(controller.listAll);
-router.route('/single/:id').get(controller.getByNid);
-router.route('/single/:id/:page').get(controller.getByNidPage);
+router.route('/').get(cache(), controller.listAllPage);
+router.route('/:page').get(cache(), controller.listAll);
+router.route('/single/:id').get(cache(), controller.getByNid);
+router.route('/single/:id/:page').get(cache(), controller.getByNidPage);
 
 module.exports = router;

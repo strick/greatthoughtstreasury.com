7a6dcfede00a0566ce04167d8bdb88687bac1c78
Brian <bstrick@gmail.com>
2021-06-02 15:53:54 -0400

    dont use cache for quotes AB#420

--- gtt-web/src/routes/quotes.js
+++ gtt-web/src/routes/quotes.js
@@ -10,10 +10,16 @@ var cache = require("express-redis-cache")({
     auth_pass: process.env.REDISCACHEKEY
   });
 
+router.route('/').get(controller.listAllPage);
+router.route('/:page').get(controller.listAll);
+router.route('/single/:id').get(controller.getByNid);
+router.route('/related/:id').get(controller.related);
+router.route('/related/:id/:page').get(controller.related);
+/*
 router.route('/').get(cache.route(), controller.listAllPage);
 router.route('/:page').get(cache.route(), controller.listAll);
 router.route('/single/:id').get(cache.route(), controller.getByNid);
 router.route('/related/:id').get(cache.route(), controller.related);
 router.route('/related/:id/:page').get(cache.route(), controller.related);
-
+*/
 module.exports = router;

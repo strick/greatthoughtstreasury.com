5d9b965a107d7fe69f99a19d4c8bc7e88977a52b
Brian <bstrick@gmail.com>
2021-06-01 21:10:36 -0400

    Add caching back AB#416

--- gtt-web/src/routes/quotes.js
+++ gtt-web/src/routes/quotes.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var cache = require('../middleware/cache');
 var controller = require('../controllers/quote');
 var router = express.Router();
 
 
-router.route('/').get(controller.listAllPage);
-router.route('/:page').get(controller.listAll);
-router.route('/single/:id').get(controller.getByNid);
-router.route('/related/:id').get(controller.related);
-router.route('/related/:id/:page').get(controller.related);
+router.route('/').get(cache(), controller.listAllPages);
+router.route('/:page').get(cache(), controller.listAll);
+router.route('/single/:id').get(cache(), controller.getByNid);
+router.route('/related/:id').get(cache(), controller.related);
+router.route('/related/:id/:page').get(cache(), controller.related);
 
 module.exports = router;

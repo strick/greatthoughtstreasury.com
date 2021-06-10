7c9b7d6a9916a536bfd24bd234086bd9ab4c0593
Brian <bstrick@gmail.com>
2021-06-10 12:06:46 -0400

    Removing old files

--- gtt-web/src/routes/author.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var cache = require('../middleware/cache');
-var controller = require('../controllers/author');
-var router = express.Router();
-
-router.route('/:slug').get(cache(), controller.getBySlug);
-
-module.exports = router;

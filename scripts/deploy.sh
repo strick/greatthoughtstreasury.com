569142f0fa210bb84f01aa6f09f82d94f652a3fd
Brian <bstrick@gmail.com>
2021-06-10 10:12:40 -0400

    add back

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+cd tf
+
+echo "terraform apply -auto-approve\"
+    -var \"app_version=$VERSION\" 
+    -var \"client_id=$ARM_CLIENT_ID\" 
+    -var \"client_secret=$ARM_CLIENT_SECRET\""
+
+echo  
+
+terraform init 
+terraform apply -auto-approve \
+    -var "app_version=$VERSION" \
+    -var "client_id=$ARM_CLIENT_ID" \
+    -var "client_secret=$ARM_CLIENT_SECRET" \
+    -var "tenant_id=$ARM_TENANT_ID" \
+    -var "subscription_id=$ARM_SUBSCRIPTION_ID"

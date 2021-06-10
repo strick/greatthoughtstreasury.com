069dcd28241090793f4c6f85a539fa12f4382675
Brian <bstrick@gmail.com>
2021-06-10 10:00:25 -0400

    removing tf folder since it has its own repo now

--- tf/main.tf
+++
@@ -1,9 +0,0 @@
-# Initialises Terraform providers and sets their version numbers.
-
-provider "azurerm" {
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
-  features {}
-}
--- tf/resource-group.tf
+++
@@ -1,7 +0,0 @@
-
-resource "azurerm_resource_group" "greatthoughtstreasury" {
-
-    name = var.app_name
-
-    location = var.location
-}
